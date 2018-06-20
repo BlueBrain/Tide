BlueBrain/Tide

commit 8e0c7136cf458e0bbc0406d73f294b2503793f77

Improve encapsulation of Content class

tide/core/scene/Content.cpp
@@ -49,19 +49,11 @@ qreal Content::_maxScale = 3.0;
 
 Content::Content(const QString& uri)
     : _uri(uri)
-    , _zoomRect(UNIT_RECTF)
-    , _actions(this)
-    , _keyboardState(this)
-    , _captureInteraction(false)
 {
     _init();
 }
 
 Content::Content()
-    : _zoomRect(UNIT_RECTF)
-    , _actions(this)
-    , _keyboardState(this)
-    , _captureInteraction(false)
 {
     _init();
 }
@@ -198,7 +190,6 @@ void Content::setDimensions(const QSize& dimensions)
         return;
 
     _size = dimensions;
-    emit dimensionsChanged();
     emit modified();
 }
 

tide/core/scene/Content.h
@@ -65,14 +65,18 @@ class Content : public QObject
     Q_OBJECT
     Q_DISABLE_COPY(Content)
 
+    // These properties are read-only on master and wall
     Q_PROPERTY(QString uri READ getURI CONSTANT)
     Q_PROPERTY(QString title READ getTitle NOTIFY titleChanged)
-    Q_PROPERTY(QSize size READ getDimensions NOTIFY dimensionsChanged)
-    Q_PROPERTY(qreal aspectRatio READ getAspectRatio CONSTANT)
     Q_PROPERTY(bool hasFixedAspectRatio READ hasFixedAspectRatio CONSTANT)
-    Q_PROPERTY(QRectF zoomRect READ getZoomRect CONSTANT)
-    Q_PROPERTY(ContentActionsModel* actions READ getActions CONSTANT)
     Q_PROPERTY(QString qmlControls READ getQmlControls CONSTANT)
+
+    // These properties can be constant because they are only accessed on wall
+    Q_PROPERTY(qreal aspectRatio READ getAspectRatio CONSTANT)
+    Q_PROPERTY(QRectF zoomRect READ getZoomRect CONSTANT)
+
+    // These properties are written by master and read on wall
+    Q_PROPERTY(ContentActionsModel* actions READ getActions CONSTANT)
     Q_PROPERTY(KeyboardState* keyboard READ getKeyboardState CONSTANT)
     Q_PROPERTY(Interaction interactionPolicy READ getInteractionPolicy NOTIFY
                    interactionPolicyChanged)
@@ -185,17 +189,17 @@ signals:
     void titleChanged(QString title);
     void interactionPolicyChanged();
     void captureInteractionChanged();
-    void dimensionsChanged();
     //@}
 
     /** Emitted by any Content subclass when its state has been modified */
     void modified();
 
 protected:
-    friend class boost::serialization::access;
-
     // Default constructor required for boost::serialization
     Content();
+
+private:
+    friend class boost::serialization::access;
 
     /** Serialize for sending to Wall applications. */
     template <class Archive>
@@ -249,11 +253,11 @@ protected:
 
     QString _uri;
     QSize _size;
-    QRectF _zoomRect;
-    ContentActionsModel _actions;
-    KeyboardState _keyboardState;
+    QRectF _zoomRect{UNIT_RECTF};
     deflect::SizeHints _sizeHints;
-    bool _captureInteraction;
+    bool _captureInteraction{false};
+    ContentActionsModel _actions{this};
+    KeyboardState _keyboardState{this};
 
     static qreal _maxScale;
 };

tide/core/scene/ContentFactory.cpp
@@ -1,5 +1,5 @@
 /*********************************************************************/
-/* Copyright (c) 2013-2016, EPFL/Blue Brain Project                  */
+/* Copyright (c) 2013-2018, EPFL/Blue Brain Project                  */
 /*                          Raphael Dumusc <raphael.dumusc@epfl.ch>  */
 /* All rights reserved.                                              */
 /*                                                                   */
@@ -96,6 +96,19 @@ ContentPtr _makeContent(const QString& uri)
         return nullptr;
     }
 }
+
+class ErrorContent : public TextureContent
+{
+public:
+    ErrorContent(const QSize& size)
+        : TextureContent(ERROR_IMAGE_FILENAME)
+    {
+        if (!size.isEmpty())
+            setDimensions(size);
+        else
+            readMetadata();
+    }
+};
 }
 
 CONTENT_TYPE ContentFactory::getContentTypeForFile(const QString& uri)
@@ -159,12 +172,13 @@ ContentPtr ContentFactory::getContent(const QString& uri)
 }
 
 ContentPtr ContentFactory::getPixelStreamContent(const QString& uri,
-                                                 StreamType stream)
+                                                 const QSize& size,
+                                                 const StreamType stream)
 {
     if (stream == StreamType::WEBBROWSER)
     {
 #if TIDE_ENABLE_WEBBROWSER_SUPPORT
-        return ContentPtr(new WebbrowserContent(uri));
+        return std::make_unique<WebbrowserContent>(uri, size);
 #else
         Q_UNUSED(uri);
         throw std::runtime_error("Tide compiled without WebbrowserContent!");
@@ -173,18 +187,13 @@ ContentPtr ContentFactory::getPixelStreamContent(const QString& uri,
     else
     {
         const auto keyboard = stream == StreamType::EXTERNAL;
-        return ContentPtr(new PixelStreamContent(uri, keyboard));
+        return std::make_unique<PixelStreamContent>(uri, size, keyboard);
     }
 }
 
 ContentPtr ContentFactory::getErrorContent(const QSize& size)
 {
-    ContentPtr content(new TextureContent(ERROR_IMAGE_FILENAME));
-    if (!size.isEmpty())
-        content->setDimensions(size);
-    else
-        content->readMetadata();
-    return content;
+    return std::make_unique<ErrorContent>(size);
 }
 
 const QStringList& ContentFactory::getSupportedExtensions()

tide/core/scene/ContentFactory.h
@@ -1,5 +1,5 @@
 /*********************************************************************/
-/* Copyright (c) 2013-2016, EPFL/Blue Brain Project                  */
+/* Copyright (c) 2013-2018, EPFL/Blue Brain Project                  */
 /*                          Raphael Dumusc <raphael.dumusc@epfl.ch>  */
 /* All rights reserved.                                              */
 /*                                                                   */
@@ -54,7 +54,8 @@ public:
 
     /** Special case: PixelStreamContent type cannot be derived from its uri. */
     static ContentPtr getPixelStreamContent(
-        const QString& uri, StreamType stream = StreamType::EXTERNAL);
+        const QString& uri, const QSize& size,
+        StreamType stream = StreamType::EXTERNAL);
 
     /** Get a Content object representing a loading error. */
     static ContentPtr getErrorContent(const QSize& size = QSize());

tide/core/scene/ImagePyramidContent.cpp
@@ -57,7 +57,7 @@ bool ImagePyramidContent::readMetadata()
 {
     try
     {
-        _size = TiffPyramidReader{_uri}.getImageSize();
+        setDimensions(TiffPyramidReader{getURI()}.getImageSize());
     }
     catch (...)
     {

tide/core/scene/SVGContent.cpp
@@ -67,7 +67,7 @@ bool SVGContent::readMetadata()
     if (!svg.isValid())
         return false;
 
-    _size = svg.getSize();
+    setDimensions(svg.getSize());
     return true;
 }
 

commit 10b5453457df053a9a57900948f064f5ea5a8662

Improve handling of error contents
- Display the title / file path of the original content.
- Don't save error contents in sessions.
- Fix a crash introduced in 8e0c713 (missing boost::serialization for
  ErrorContent class).

tide/core/CMakeLists.txt
@@ -154,6 +154,7 @@ list(APPEND TIDECORE_PUBLIC_HEADERS
   scene/CountdownStatus.h
   scene/DisplayGroup.h
   scene/DynamicTextureContent.h
+  scene/ErrorContent.h
   scene/KeyboardState.h
   scene/Markers.h
   scene/Options.h
@@ -215,6 +216,7 @@ list(APPEND TIDECORE_SOURCES
   scene/CountdownStatus.cpp
   scene/DisplayGroup.cpp
   scene/DynamicTextureContent.cpp
+  scene/ErrorContent.cpp
   scene/KeyboardState.cpp
   scene/Markers.cpp
   scene/Options.cpp

tide/core/resources/BaseContentWindow.qml
@@ -64,7 +64,7 @@ Rectangle {
 
                     fontSizeMode: options.showFilePaths ? Text.Fit : Text.FixedSize
                     elide: options.showFilePaths ? Text.ElideLeft : Text.ElideRight
-                    text: options.showFilePaths ? contentwindow.content.uri : contentwindow.content.title
+                    text: options.showFilePaths ? contentwindow.content.filePath : contentwindow.content.title
                     font { family: "qrc::gotham-book"; pixelSize: Style.windowTitleFontSize }
                 }
             }

tide/core/scene/Content.cpp
@@ -77,9 +77,14 @@ const QString& Content::getURI() const
     return _uri;
 }
 
+QString Content::getFilePath() const
+{
+    return getURI();
+}
+
 QString Content::getTitle() const
 {
-    return getURI().section("/", -1, -1);
+    return getFilePath().section("/", -1, -1);
 }
 
 QSize Content::getDimensions() const

tide/core/scene/Content.h
@@ -67,6 +67,7 @@ class Content : public QObject
 
     // These properties are read-only on master and wall
     Q_PROPERTY(QString uri READ getURI CONSTANT)
+    Q_PROPERTY(QString filePath READ getFilePath CONSTANT)
     Q_PROPERTY(QString title READ getTitle NOTIFY titleChanged)
     Q_PROPERTY(bool hasFixedAspectRatio READ hasFixedAspectRatio CONSTANT)
     Q_PROPERTY(QString qmlControls READ getQmlControls CONSTANT)
@@ -104,6 +105,9 @@ public:
 
     /** Get the content type **/
     virtual CONTENT_TYPE getType() const = 0;
+
+    /** @return the full path to the content file. */
+    virtual QString getFilePath() const;
 
     /** Get the title of the content */
     virtual QString getTitle() const;

tide/core/scene/ContentFactory.cpp
@@ -43,6 +43,10 @@
 #include "log.h"
 
 #include "Content.h"
+#include "ErrorContent.h"
+#include "PixelStreamContent.h"
+#include "SVGContent.h"
+#include "TextureContent.h"
 #if TIDE_USE_TIFF
 #include "ImagePyramidContent.h"
 #include "data/TiffPyramidReader.h"
@@ -53,9 +57,6 @@
 #if TIDE_ENABLE_PDF_SUPPORT
 #include "PDFContent.h"
 #endif
-#include "PixelStreamContent.h"
-#include "SVGContent.h"
-#include "TextureContent.h"
 #if TIDE_ENABLE_WEBBROWSER_SUPPORT
 #include "WebbrowserContent.h"
 #endif
@@ -64,8 +65,6 @@
 #include <QFileInfo>
 #include <QImageReader>
 #include <QTextStream>
-
-#define ERROR_IMAGE_FILENAME ":/img/error.png"
 
 namespace
 {
@@ -96,19 +95,6 @@ ContentPtr _makeContent(const QString& uri)
         return nullptr;
     }
 }
-
-class ErrorContent : public TextureContent
-{
-public:
-    ErrorContent(const QSize& size)
-        : TextureContent(ERROR_IMAGE_FILENAME)
-    {
-        if (!size.isEmpty())
-            setDimensions(size);
-        else
-            readMetadata();
-    }
-};
 }
 
 CONTENT_TYPE ContentFactory::getContentTypeForFile(const QString& uri)
@@ -191,9 +177,17 @@ ContentPtr ContentFactory::getPixelStreamContent(const QString& uri,
     }
 }
 
-ContentPtr ContentFactory::getErrorContent(const QSize& size)
+ContentPtr ContentFactory::getErrorContent(const Content& content)
 {
-    return std::make_unique<ErrorContent>(size);
+    const auto& uri = content.getURI();
+    const auto& size = content.getDimensions();
+
+    return std::make_unique<ErrorContent>(uri, size);
+}
+
+ContentPtr ContentFactory::getErrorContent(const QString& uri)
+{
+    return std::make_unique<ErrorContent>(uri, QSize());
 }
 
 const QStringList& ContentFactory::getSupportedExtensions()

tide/core/scene/ContentFactory.h
@@ -58,7 +58,10 @@ public:
         StreamType stream = StreamType::EXTERNAL);
 
     /** Get a Content object representing a loading error. */
-    static ContentPtr getErrorContent(const QSize& size = QSize());
+    static ContentPtr getErrorContent(const Content& content);
+
+    /** For legacy sessions; deprecated. */
+    static ContentPtr getErrorContent(const QString& uri);
 
     /** Get all the supported file extensions. */
     static const QStringList& getSupportedExtensions();

tide/core/scene/ErrorContent.cpp
@@ -1,8 +1,6 @@
 /*********************************************************************/
-/* Copyright (c) 2011 - 2012, The University of Texas at Austin.     */
-/* Copyright (c) 2013-2016, EPFL/Blue Brain Project                  */
-/*                     Raphael.Dumusc@epfl.ch                        */
-/*                     Daniel.Nachbaur@epfl.ch                       */
+/* Copyright (c) 2018, EPFL/Blue Brain Project                       */
+/*                     Raphael Dumusc <raphael.dumusc@epfl.ch>       */
 /* All rights reserved.                                              */
 /*                                                                   */
 /* Redistribution and use in source and binary forms, with or        */
@@ -39,11 +37,26 @@
 /* or implied, of Ecole polytechnique federale de Lausanne.          */
 /*********************************************************************/
 
-#include "DynamicTextureContent.h"
+#include "ErrorContent.h"
 
-BOOST_CLASS_EXPORT(DynamicTextureContent)
+BOOST_CLASS_EXPORT(ErrorContent)
 
-CONTENT_TYPE DynamicTextureContent::getType() const
+namespace
 {
-    return CONTENT_TYPE_DYNAMIC_TEXTURE;
+const auto ERROR_IMAGE_FILENAME = ":/img/error.png";
+}
+
+ErrorContent::ErrorContent(const QString& uri, const QSize& size)
+    : TextureContent(ERROR_IMAGE_FILENAME)
+    , _originalUri{uri}
+{
+    if (!size.isEmpty())
+        setDimensions(size);
+    else
+        readMetadata();
+}
+
+QString ErrorContent::getFilePath() const
+{
+    return _originalUri;
 }

tide/core/scene/ErrorContent.h
@@ -1,8 +1,6 @@
 /*********************************************************************/
-/* Copyright (c) 2011 - 2012, The University of Texas at Austin.     */
-/* Copyright (c) 2013-2016, EPFL/Blue Brain Project                  */
-/*                     Raphael.Dumusc@epfl.ch                        */
-/*                     Daniel.Nachbaur@epfl.ch                       */
+/* Copyright (c) 2018, EPFL/Blue Brain Project                       */
+/*                     Raphael Dumusc <raphael.dumusc@epfl.ch>       */
 /* All rights reserved.                                              */
 /*                                                                   */
 /* Redistribution and use in source and binary forms, with or        */
@@ -39,11 +37,45 @@
 /* or implied, of Ecole polytechnique federale de Lausanne.          */
 /*********************************************************************/
 
-#include "DynamicTextureContent.h"
+#ifndef ERROR_CONTENT_H
+#define ERROR_CONTENT_H
 
-BOOST_CLASS_EXPORT(DynamicTextureContent)
+#include "TextureContent.h"
 
-CONTENT_TYPE DynamicTextureContent::getType() const
+/**
+ * A placeholder to replace a Content that can no longer be restored from file.
+ *
+ * Error contents are not kept when saving the session again.
+ */
+class ErrorContent : public TextureContent
 {
-    return CONTENT_TYPE_DYNAMIC_TEXTURE;
-}
+public:
+    /**
+     * Constructor.
+     * @param uri The uri of the original content.
+     * @param size The size of the original content.
+     */
+    ErrorContent(const QString& uri, const QSize& size);
+
+    /** @return the uri of the original content. */
+    QString getFilePath() const override;
+
+private:
+    QString _originalUri;
+
+    friend class boost::serialization::access;
+
+    ErrorContent() = default; // required for boost::serialization
+
+    template <class Archive>
+    void serialize(Archive& ar, const unsigned int)
+    {
+        // serialize base class information (with NVP for xml archives)
+        // clang-format off
+        ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(TextureContent);
+        ar & boost::serialization::make_nvp("originalUri", _originalUri);
+        // clang-format on
+    }
+};
+
+#endif

tide/core/scene/ImagePyramidContent.cpp
@@ -41,7 +41,7 @@
 
 #include "data/TiffPyramidReader.h"
 
-BOOST_CLASS_EXPORT_GUID(ImagePyramidContent, "ImagePyramidContent")
+BOOST_CLASS_EXPORT(ImagePyramidContent)
 
 ImagePyramidContent::ImagePyramidContent(const QString& uri)
     : Content(uri)

tide/core/scene/SVGContent.cpp
@@ -45,7 +45,7 @@
 
 #include <QFileInfo>
 
-BOOST_CLASS_EXPORT_GUID(SVGContent, "SVGContent")
+BOOST_CLASS_EXPORT(SVGContent)
 
 SVGContent::SVGContent(const QString& uri)
     : VectorialContent(uri)

tide/core/scene/TextureContent.cpp
@@ -43,7 +43,7 @@
 
 #include <QImageReader>
 
-BOOST_CLASS_EXPORT_GUID(TextureContent, "TextureContent")
+BOOST_CLASS_EXPORT_IMPLEMENT(TextureContent)
 
 TextureContent::TextureContent(const QString& uri)
     : Content(uri)

tide/core/scene/TextureContent.h
@@ -67,11 +67,12 @@ public:
 
     static const QStringList& getSupportedExtensions();
 
+protected:
+    TextureContent() = default; // required for boost::serialization
+
 private:
     friend class boost::serialization::access;
 
-    // Default constructor required for boost::serialization
-    TextureContent() {}
     template <class Archive>
     void serialize(Archive& ar, const unsigned int)
     {
@@ -81,5 +82,8 @@ private:
         // clang-format on
     }
 };
+
+// Need to be in header for serialization of derived class ErrorContent
+BOOST_CLASS_EXPORT_KEY(TextureContent)
 
 #endif

tide/master/State.cpp
@@ -77,7 +77,7 @@ ContentPtr _loadContent(XmlParser& parser, const QString& index)
     if (parser.get(query.arg(index, "URI"), uri))
         content = ContentFactory::getContent(uri);
     if (!content)
-        content = ContentFactory::getErrorContent();
+        content = ContentFactory::getErrorContent(uri);
 
     return content;
 }

tide/master/StateSerializationHelper.cpp
@@ -44,6 +44,7 @@
 #include "control/DisplayGroupController.h"
 #include "log.h"
 #include "scene/ContentFactory.h"
+#include "scene/ErrorContent.h"
 #include "scene/Scene.h"
 #include "serialization/utils.h"
 
@@ -60,6 +61,16 @@ bool _canBeRestored(const CONTENT_TYPE type)
         return false;
 
     return true;
+}
+
+bool _isErrorContent(const Content& content)
+{
+    return !!dynamic_cast<const ErrorContent*>(&content);
+}
+
+bool _canBeSaved(const Content& content)
+{
+    return _canBeRestored(content.getType()) && !_isErrorContent(content);
 }
 
 void _relocateContent(ContentWindow& window, const QString& tmpDir,
@@ -176,8 +187,7 @@ bool _validateContent(const ContentWindowPtr& window)
     {
         print_log(LOG_WARN, LOG_CONTENT, "'%s' could not be restored!",
                   content->getURI().toLocal8Bit().constData());
-        const auto& size = content->getDimensions();
-        auto errorContent = ContentFactory::getErrorContent(size);
+        auto errorContent = ContentFactory::getErrorContent(*content);
         errorContent->moveToThread(window->thread());
         window->setContent(std::move(errorContent));
     }
@@ -228,14 +238,14 @@ StateSerializationHelper::StateSerializationHelper(ScenePtr scene)
 {
 }
 
-SceneConstPtr _load(const QString& filename, SceneConstPtr referenceScene)
+ScenePtr _load(const QString& filename, SceneConstPtr referenceScene)
 {
     State state;
     // For backward compatibility, try to load the file as a legacy xml first
     if (!state.legacyLoadXML(filename) &&
         !serialization::fromXmlFile(state, filename.toStdString()))
     {
-        return SceneConstPtr();
+        return ScenePtr();
     }
 
     auto scene = state.getScene();
@@ -263,8 +273,7 @@ SceneConstPtr _load(const QString& filename, SceneConstPtr referenceScene)
     return scene;
 }
 
-QFuture<SceneConstPtr> StateSerializationHelper::load(
-    const QString& filename) const
+QFuture<ScenePtr> StateSerializationHelper::load(const QString& filename) const
 {
     print_log(LOG_INFO, LOG_CONTENT, "Restoring session: '%s'",
               filename.toStdString().c_str());
@@ -295,7 +304,7 @@ void _filterContents(DisplayGroup& group)
     std::copy_if(windows.begin(), windows.end(),
                  std::back_inserter(filteredWindows),
                  [](const ContentWindowPtr& window) {
-                     return _canBeRestored(window->getContent().getType());
+                     return _canBeSaved(window->getContent());
                  });
     group.setContentWindows(filteredWindows);
 }

tide/master/StateSerializationHelper.h
@@ -77,7 +77,7 @@ public:
      *
      * @return the loaded scene on success, nullptr on failure.
      */
-    QFuture<SceneConstPtr> load(const QString& filename) const;
+    QFuture<ScenePtr> load(const QString& filename) const;
 
     /**
      * Find an available filename by appending "_[n]" if needed to the filename.

tide/master/control/SceneController.cpp
@@ -60,16 +60,15 @@ SceneController::SceneController(Scene& scene_,
                 &SceneController::_deleteTempContentFile);
     }
 
-    connect(&_loadSessionOp, &QFutureWatcher<SceneConstPtr>::finished,
-            [this]() {
-                auto scene = _loadSessionOp.result();
-                if (scene)
-                    apply(scene);
+    connect(&_loadSessionOp, &QFutureWatcher<ScenePtr>::finished, [this]() {
+        auto scene = _loadSessionOp.result();
+        if (scene)
+            apply(scene);
 
-                if (_loadSessionCallback)
-                    _loadSessionCallback(scene != nullptr);
-                _loadSessionCallback = nullptr;
-            });
+        if (_loadSessionCallback)
+            _loadSessionCallback(scene != nullptr);
+        _loadSessionCallback = nullptr;
+    });
 
     connect(&_saveSessionOp, &QFutureWatcher<bool>::finished, [this]() {
         if (_saveSessionCallback)

tide/master/control/SceneController.h
@@ -80,7 +80,7 @@ private:
     Scene& _scene;
     Configuration::Folders _folders;
 
-    QFutureWatcher<SceneConstPtr> _loadSessionOp;
+    QFutureWatcher<ScenePtr> _loadSessionOp;
     QFutureWatcher<bool> _saveSessionOp;
     BoolCallback _loadSessionCallback;
     BoolCallback _saveSessionCallback;

tide/master/ui/MasterWindow.cpp
@@ -93,15 +93,13 @@ MasterWindow::MasterWindow(ScenePtr scene_, OptionsPtr options,
             });
 #endif
 
-    connect(&_loadSessionOp, &QFutureWatcher<SceneConstPtr>::finished,
-            [this]() {
-                if (auto scene = _loadSessionOp.result())
-                    emit sessionLoaded(scene);
-                else
-                    QMessageBox::warning(this, "Error",
-                                         "Could not load session file.",
-                                         QMessageBox::Ok, QMessageBox::Ok);
-            });
+    connect(&_loadSessionOp, &QFutureWatcher<ScenePtr>::finished, [this]() {
+        if (auto scene = _loadSessionOp.result())
+            emit sessionLoaded(scene);
+        else
+            QMessageBox::warning(this, "Error", "Could not load session file.",
+                                 QMessageBox::Ok, QMessageBox::Ok);
+    });
 
     connect(&_saveSessionOp, &QFutureWatcher<bool>::finished, [this]() {
         if (!_saveSessionOp.result())

tide/master/ui/MasterWindow.h
@@ -76,7 +76,7 @@ signals:
     void openWhiteboard(uint surfaceIndex);
 
     /** Emitted when a session has been successfully loaded. */
-    void sessionLoaded(SceneConstPtr group);
+    void sessionLoaded(ScenePtr group);
 
 protected:
     /** @name Drag events re-implemented from QMainWindow */
@@ -112,7 +112,7 @@ private:
     ScenePtr _scene;
     OptionsPtr _options;
 
-    QFutureWatcher<SceneConstPtr> _loadSessionOp;
+    QFutureWatcher<ScenePtr> _loadSessionOp;
     QFutureWatcher<bool> _saveSessionOp;
 
     BackgroundWidget* _backgroundWidget; // child QObject

tests/cpp/core/DummyContent.h
@@ -79,6 +79,6 @@ private:
     }
 };
 
-BOOST_CLASS_EXPORT_GUID(DummyContent, "DummyContent")
+BOOST_CLASS_EXPORT(DummyContent)
 
 #endif

tests/cpp/core/StateSerializationTests.cpp
@@ -46,6 +46,7 @@
 #include "scene/ContentFactory.h"
 #include "scene/ContentWindow.h"
 #include "scene/DisplayGroup.h"
+#include "scene/ErrorContent.h"
 #include "scene/TextureContent.h"
 #include "serialization/utils.h"
 #include "types.h"
@@ -77,6 +78,20 @@ const QString STATE_V3_NOTITLES_URI = "state_v3_noTitles.dcx";
 const QString STATE_V4_URI = "state_v4.dcx";
 const QString TEST_DIR = "tmp";
 const QSize VALID_TEXTURE_SIZE(256, 128);
+}
+
+ContentWindowPtr makeValidTestWindow(const QString& uri)
+{
+    auto content = ContentFactory::getContent(uri);
+    BOOST_REQUIRE(content);
+    BOOST_REQUIRE_EQUAL(content->getDimensions(), VALID_TEXTURE_SIZE);
+    return std::make_shared<ContentWindow>(std::move(content));
+}
+
+ContentWindowPtr makeInvalidTestWindow(const QString& uri)
+{
+    auto missingContent = std::make_unique<TextureContent>(uri);
+    return std::make_shared<ContentWindow>(std::move(missingContent));
 }
 
 ScenePtr makeTestScene()
@@ -335,15 +350,13 @@ BOOST_AUTO_TEST_CASE(testStateSerializationHelperReadingFromVersion4File)
 
 DisplayGroupPtr createTestDisplayGroup()
 {
-    auto content = ContentFactory::getContent(VALID_TEXTURE_URI);
-    BOOST_REQUIRE(content);
-    BOOST_REQUIRE_EQUAL(content->getDimensions(), VALID_TEXTURE_SIZE);
+    auto window = makeValidTestWindow(VALID_TEXTURE_URI);
 
-    ContentWindowPtr window(new ContentWindow(std::move(content)));
     const QPointF position(0.25 * wallSize.width(), 0.25 * wallSize.height());
     window->setCoordinates(QRectF(position, 0.5 * wallSize));
     window->getContent().setZoomRect(QRectF(0.1, 0.2, 1.0 / 1.5, 1.0 / 1.5));
-    DisplayGroupPtr displayGroup(new DisplayGroup(wallSize));
+
+    auto displayGroup = DisplayGroup::create(wallSize);
     displayGroup->addContentWindow(window);
     return displayGroup;
 }
@@ -359,13 +372,23 @@ void cleanup(const QString& dir)
         QFile::remove(dir + "/" + file);
 }
 
-BOOST_AUTO_TEST_CASE(testStateSerializationToFile)
+void setupTestDir()
 {
-    // 1) Setup
     if (!QDir().mkdir(TEST_DIR))
         cleanup(TEST_DIR);
     // empty folders contain 2 elements: '.' and '..'
     BOOST_REQUIRE_EQUAL(QDir(TEST_DIR).count(), 2);
+}
+
+void cleanupTestDir()
+{
+    QDir(TEST_DIR).removeRecursively();
+}
+
+BOOST_AUTO_TEST_CASE(testStateSerializationToFile)
+{
+    // 1) Setup
+    setupTestDir();
 
     // 2) Test saving
     auto displayGroup = createTestDisplayGroup();
@@ -396,7 +419,7 @@ BOOST_AUTO_TEST_CASE(testStateSerializationToFile)
     checkWindow(loadedGroup.getContentWindows()[0]);
 
     // 4) Cleanup
-    QDir(TEST_DIR).removeRecursively();
+    cleanupTestDir();
 }
 
 BOOST_AUTO_TEST_CASE(testStateSerializationUploadedToFile)
@@ -415,10 +438,7 @@ BOOST_AUTO_TEST_CASE(testStateSerializationUploadedToFile)
 
     // 3) Test saving
     auto displayGroup = createTestDisplayGroup();
-    auto content = ContentFactory::getContent(newValidUri);
-    BOOST_REQUIRE(content);
-    BOOST_REQUIRE_EQUAL(content->getDimensions(), VALID_TEXTURE_SIZE);
-    auto contentWindow = std::make_shared<ContentWindow>(std::move(content));
+    auto contentWindow = makeValidTestWindow(newValidUri);
     displayGroup->addContentWindow(contentWindow);
     StateSerializationHelper helper{Scene::create(displayGroup)};
     BOOST_CHECK(
@@ -434,8 +454,7 @@ BOOST_AUTO_TEST_CASE(testStateSerializationUploadedToFile)
 
     // 4) Add another file with the same name and test saving
     QFile::copy(VALID_TEXTURE_URI, newValidUri);
-    content = ContentFactory::getContent(newValidUri);
-    auto newContentWindow = std::make_shared<ContentWindow>(std::move(content));
+    auto newContentWindow = makeValidTestWindow(newValidUri);
     displayGroup->addContentWindow(newContentWindow);
     BOOST_CHECK(
         helper.save(uploadDir + "test.dcx", tempDir, uploadDir).result());
@@ -447,4 +466,61 @@ BOOST_AUTO_TEST_CASE(testStateSerializationUploadedToFile)
                       savedSessionDir + "uploaded_1.png");
 
     QDir{uploadDir}.removeRecursively();
+}
+
+BOOST_AUTO_TEST_CASE(testErrorContentWhenFileMissing)
+{
+    // 1) setup
+    setupTestDir();
+    const auto file = TEST_DIR + "/missing_content.dcx";
+
+    auto group = DisplayGroup::create(wallSize);
+    group->addContentWindow(makeInvalidTestWindow(INVALID_URI));
+    group->addContentWindow(makeValidTestWindow(VALID_TEXTURE_URI));
+    auto scene = Scene::create(group);
+
+    // 2) saving session
+    BOOST_CHECK(StateSerializationHelper{scene}.save(file).result());
+
+    // 3) check restoring session
+    auto loader = StateSerializationHelper{scene};
+    auto loadedScene = loader.load(file).result();
+    BOOST_REQUIRE(loadedScene);
+    const auto& loadedGroup = loadedScene->getGroup(0);
+
+    BOOST_REQUIRE_EQUAL(loadedGroup.getContentWindows().size(),
+                        group->getContentWindows().size());
+
+    const auto& errorContent = loadedGroup.getContentWindows()[0]->getContent();
+    {
+        // WAR compiler warnings with BOOST_CHECK_NO_THROW macro
+        const auto e = dynamic_cast<const ErrorContent*>(&errorContent);
+        BOOST_CHECK(e);
+    }
+    BOOST_CHECK_EQUAL(errorContent.getURI(), ":/img/error.png");
+    BOOST_CHECK_EQUAL(errorContent.getFilePath(), INVALID_URI);
+    BOOST_CHECK_EQUAL(errorContent.getTitle(), "uri");
+
+    const auto& validContent = loadedGroup.getContentWindows()[1]->getContent();
+    BOOST_CHECK_EQUAL(validContent.getURI(), VALID_TEXTURE_URI);
+    BOOST_CHECK_EQUAL(validContent.getFilePath(), VALID_TEXTURE_URI);
+    BOOST_CHECK_EQUAL(validContent.getTitle(), VALID_TEXTURE_URI);
+
+    // 4) saving session a second time (error content is not saved)
+    BOOST_CHECK(StateSerializationHelper{loadedScene}.save(file).result());
+
+    // 5) check restoring session a second time
+    loadedScene = loader.load(file).result();
+    BOOST_REQUIRE(loadedScene);
+    const auto& loadedGroup2 = loadedScene->getGroup(0);
+
+    BOOST_REQUIRE_EQUAL(loadedGroup2.getContentWindows().size(), 1);
+    const auto& validContent2 =
+        loadedGroup2.getContentWindows()[0]->getContent();
+    BOOST_CHECK_EQUAL(validContent2.getURI(), VALID_TEXTURE_URI);
+    BOOST_CHECK_EQUAL(validContent2.getFilePath(), VALID_TEXTURE_URI);
+    BOOST_CHECK_EQUAL(validContent2.getTitle(), VALID_TEXTURE_URI);
+
+    // 5) cleanup
+    cleanupTestDir();
 }