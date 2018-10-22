258abb2c1fde90b634b27d49abde1831fb0377c0
Raphael Dumusc <raphael.dumusc@epfl.ch>
2018-10-22 15:00:23 +0200

    Refactor MasterApplication code into new MarkersUpdater

---
+++ tide/master/tools/MarkersUpdater.h
@@ -0,0 +1,77 @@
+/*********************************************************************/
+/* Copyright (c) 2018, EPFL/Blue Brain Project                       */
+/*                     Raphael Dumusc <raphael.dumusc@epfl.ch>       */
+/* All rights reserved.                                              */
+/*                                                                   */
+/* Redistribution and use in source and binary forms, with or        */
+/* without modification, are permitted provided that the following   */
+/* conditions are met:                                               */
+/*                                                                   */
+/*   1. Redistributions of source code must retain the above         */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer.                                                  */
+/*                                                                   */
+/*   2. Redistributions in binary form must reproduce the above      */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer in the documentation and/or other materials       */
+/*      provided with the distribution.                              */
+/*                                                                   */
+/*    THIS  SOFTWARE IS PROVIDED  BY THE  UNIVERSITY OF  TEXAS AT    */
+/*    AUSTIN  ``AS IS''  AND ANY  EXPRESS OR  IMPLIED WARRANTIES,    */
+/*    INCLUDING, BUT  NOT LIMITED  TO, THE IMPLIED  WARRANTIES OF    */
+/*    MERCHANTABILITY  AND FITNESS FOR  A PARTICULAR  PURPOSE ARE    */
+/*    DISCLAIMED.  IN  NO EVENT SHALL THE UNIVERSITY  OF TEXAS AT    */
+/*    AUSTIN OR CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,    */
+/*    INCIDENTAL,  SPECIAL, EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES    */
+/*    (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE    */
+/*    GOODS  OR  SERVICES; LOSS  OF  USE,  DATA,  OR PROFITS;  OR    */
+/*    BUSINESS INTERRUPTION) HOWEVER CAUSED  AND ON ANY THEORY OF    */
+/*    LIABILITY, WHETHER  IN CONTRACT, STRICT  LIABILITY, OR TORT    */
+/*    (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT    */
+/*    OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF ADVISED  OF  THE    */
+/*    POSSIBILITY OF SUCH DAMAGE.                                    */
+/*                                                                   */
+/* The views and conclusions contained in the software and           */
+/* documentation are those of the authors and should not be          */
+/* interpreted as representing official policies, either expressed   */
+/* or implied, of Ecole polytechnique federale de Lausanne.          */
+/*********************************************************************/
+
+#ifndef MARKERS_UPDATER_H
+#define MARKERS_UPDATER_H
+
+#include "types.h"
+
+#include <QTouchEvent>
+
+/**
+ * The main application for the Master process.
+ */
+class MarkersUpdater
+{
+public:
+    /**
+     * Constructor
+     * @param markers The markers on which to operate.
+     * @param surfaceSize Size of the surface on which the markers are rendered.
+     */
+    MarkersUpdater(Markers& markers, const QSizeF& surfaceSize);
+
+    /** Update the touch point markers. */
+    void update(const QTouchEvent* event);
+
+    /** @name Update the mouse marker. */
+    //@{
+    void mousePressed(const QPointF& pos);
+    void mouseMoved(const QPointF& pos);
+    void mouseReleased(const QPointF& pos);
+    //@}
+
+private:
+    Markers& _markers;
+    QSizeF _surfaceSize;
+
+    QPointF _getWallPos(const QPointF& normalizedPos);
+};
+
+#endif
